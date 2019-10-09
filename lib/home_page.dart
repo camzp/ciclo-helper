6a862076195a2b4fed892282330ba849de65ce9f
camzp <camilacpbc@gmail.com>
2019-10-09 20:02:31 -0300

    Tela de Login

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,40 +2,64 @@ import 'package:ciclo_helper/authentication.dart';
 import 'package:ciclo_helper/maintenance_page.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   HomePage({this.auth, this.onSignedOut});
   final BaseAuth auth;
   final VoidCallback onSignedOut;
 
-
-  void _signOut() async{
-    try{
+  void _signOut() async {
+    try {
       await auth.signOut();
       onSignedOut();
-    }catch (e){
+    } catch (e) {
       print(e);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Ciclo Helper'),
-        actions: <Widget>[
-          new FlatButton(
-            child: new Text('Logout', style: new TextStyle(fontSize: 17.0, color: Color.fromRGBO(255, 255, 255, 1.0))),
-            onPressed: _signOut,
-          )
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 32.0,
+          ),
+          Padding(
+            padding: EdgeInsets.all(32.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(Icons.menu, color: Colors.blueGrey),
+                    Icon(Icons.settings, color: Colors.blueGrey),
+                  ],
+                ),
+                CircleAvatar(backgroundColor: Colors.pink),
+                SizedBox(height: 16.0),
+                Text(
+                  "Camila Camilo",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(height: 8.0),
+                Text("E-Bike LEV",
+                    style: TextStyle(fontSize: 12.0, color: Colors.grey)),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 32.0),
+            child: Container(
+              height: 6.0,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.topRight,
+                      colors: [Colors.lightGreen, Colors.lightGreenAccent])),
+            ),
+          ),
         ],
       ),
-      body: new Container(
-//        child: new Text('Este é o Ciclo Helper', style: new TextStyle(fontSize: 25.0))
-        child: new RaisedButton(child:new Text('Maintenance Page'), onPressed: (){
-          Navigator.push(context,MaterialPageRoute(builder:(context) => MaintenancePage()));
-        }),
-      ),
-      
     );
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'front_page.dart';
 import 'authentication.dart';
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   LoginPage({this.auth, this.onSignedIn});
   final BaseAuth auth;
   final VoidCallback onSignedIn;
@@ -10,24 +10,19 @@ class LoginPage extends StatefulWidget{
   State<StatefulWidget> createState() => new _LoginPageState();
 }
 
-enum FormType{
-  login,
-  register
-}
+enum FormType { login, register }
 
-class _LoginPageState extends State<LoginPage>{
-  
+class _LoginPageState extends State<LoginPage> {
   final formKey = new GlobalKey<FormState>();
-
 
   String _email;
   String _password;
   FormType _formType = FormType.login;
-  
+
   //verifica se os campos do formulário não estão vazios, de acordo com os validators dos campos
-  bool validateForm(){
+  bool validateForm() {
     final form = formKey.currentState;
-    if(form.validate()){
+    if (form.validate()) {
       form.save();
       return true;
     }
@@ -35,25 +30,27 @@ class _LoginPageState extends State<LoginPage>{
   }
 
   //sobe o formulário válido para autenticação do firebase
-  void validateAndSubmit() async{
-    if(validateForm()){
-      try{
-        if(_formType == FormType.login){
-          String userId = await widget.auth.signEmailPassword(_email, _password);
+  void validateAndSubmit() async {
+    if (validateForm()) {
+      try {
+        if (_formType == FormType.login) {
+          String userId =
+              await widget.auth.signEmailPassword(_email, _password);
           print('Entrou: $userId');
-        }else{
-          String userId = await widget.auth.createUserEmailPassword(_email, _password);
+        } else {
+          String userId =
+              await widget.auth.createUserEmailPassword(_email, _password);
           print('Conta criada: $userId');
         }
         widget.onSignedIn();
-      }catch (e){
+      } catch (e) {
         print("Erro: $e");
       }
     }
   }
 
- //Troca o tipo de formulario para registrar uma conta
-  void setAsRegister(){
+  //Troca o tipo de formulario para registrar uma conta
+  void setAsRegister() {
     formKey.currentState.reset();
     setState(() {
       _formType = FormType.register;
@@ -61,7 +58,7 @@ class _LoginPageState extends State<LoginPage>{
   }
 
   //Troca o tipo de formulário para logar em uma conta
-  void setAsLogin(){
+  void setAsLogin() {
     formKey.currentState.reset();
     setState(() {
       _formType = FormType.login;
@@ -71,75 +68,161 @@ class _LoginPageState extends State<LoginPage>{
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar( 
-        title: new Text('Login'),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 128,
+              height: 128,
+              child: CircleAvatar(
+                backgroundColor: Colors.cyan,
+              ),
+            ),
+            SizedBox(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: buildTextFields()),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 40,
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                child: Text(
+                  "Recuperar Senha",
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              child: SizedBox(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: buildSubmitButtons(),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      
-      body: new Container(
-        
-        padding: EdgeInsets.all(16.0),
-        
-        child: new Form(
-          key: formKey,
-          
-          child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: buildTextFields() + buildSubmitButtons()
-          ),
-        
-        )
-      
-      ),
-    
     );
-  
   }
 
   //Criando os campos de texto e os botões da página de login
 
   List<Widget> buildTextFields() {
     return [
-      new TextFormField(
-        decoration: new InputDecoration(labelText: 'Email'),
+      TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: new InputDecoration(
+          labelText: "e-mail",
+          labelStyle: TextStyle(
+            color: Colors.black38,
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+          ),
+        ),
+        style: TextStyle(
+          fontSize: 20,
+        ),
         validator: (value) => value.isEmpty ? 'Email não pode ser vazio' : null,
         onSaved: (value) => _email = value,
       ),
-              
       new TextFormField(
-        decoration: new InputDecoration(labelText: 'Senha'),
+        keyboardType: TextInputType.visiblePassword,
+        obscureText: true,
+        decoration: new InputDecoration(
+          labelText: "senha",
+          labelStyle: TextStyle(
+            color: Colors.black38,
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+          ),
+        ),
+        style: TextStyle(
+          fontSize: 20,
+        ),
         validator: (value) => value.isEmpty ? 'Senha não pode ser vazia' : null,
         onSaved: (value) => _password = value,
-        obscureText: true,
       ),
     ];
   }
 
   List<Widget> buildSubmitButtons() {
-    if(_formType == FormType.login){
+    if (_formType == FormType.login) {
       return [
-        new RaisedButton(
-          child: new Text('Entrar', style: new TextStyle(fontSize: 20.0)),
-          onPressed: validateAndSubmit,
+        Container(
+          height: 60,
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [
+                  0.3,
+                  1
+                ],
+                colors: [
+                  Colors.lightGreen,
+                  Colors.lightGreenAccent,
+                ]),
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            ),
+          ),
+          child: SizedBox.expand(
+            child: RaisedButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Entrar",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  Container(
+                    child: SizedBox(
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              onPressed: validateAndSubmit,
+            ),
+          ),
         ),
-
-        new FlatButton(
-          child: new Text('Criar uma conta', style: new TextStyle(fontSize: 20.0)),
+        FlatButton(
+          child: Text('Criar uma conta', style: TextStyle(fontSize: 20.0)),
           onPressed: setAsRegister,
         ),
       ];
-    }
-    else{
+    } else {
       return [
-        new RaisedButton(
-          child: new Text('Criar conta', style: new TextStyle(fontSize: 20.0)),
+        RaisedButton(
+          child: Text('Criar conta', style: TextStyle(fontSize: 20.0)),
           onPressed: validateAndSubmit,
         ),
-
-        new FlatButton(
-          child: new Text('Ja possui conta? Clique para entrar', style: new TextStyle(fontSize: 20.0)),
+        FlatButton(
+          child: Text('Ja possui conta? Clique para entrar',
+              style: TextStyle(fontSize: 20.0)),
           onPressed: setAsLogin,
         ),
-      ];      
+      ];
     }
   }
 }
