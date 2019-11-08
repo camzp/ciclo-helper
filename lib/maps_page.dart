96205aeb06569d2aa5201fdffb36a572de680d74
matheusmgomes <machado_gomes@hotmail.com>
2019-11-08 18:17:03 -0300

    centering map on location user and add marker button

--- lib/maps_page.dart
+++ lib/maps_page.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 //import 'package:location/location.dart';
+//import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Maps extends StatefulWidget{
@@ -12,58 +14,87 @@ class _MapsState extends State<Maps>{
 
   List<Marker> mapMarkers = [];
 
-  GoogleMapController _mapController;
-  //Location _userLocation = Location();
+  GoogleMapController mapController;
+  Position position;
+  Widget _map;
+
+  //função para ter a localização atual do usuário
+  void getCurrentLocation() async {
+    Position res = await Geolocator().getCurrentPosition();
+    setState(() {
+     position = res;
+     _map = MapWidget(); //iniciar o mapa após retornar a localização para que o LatLng fique NULL
+    });
+  }
 
   void onMapCreated(GoogleMapController ctrl){
-    _mapController = ctrl;
-    /*_userLocation.onLocationChanged().listen((l){
-      _mapController.animateCamera(
-          CameraUpdate.newCameraPosition(
-            CameraPosition(
-              target: LatLng(l.latitude, l.longitude),
-              zoom: 12.0,
-            ),
-          ),
-        );
-    });*/
+    mapController = ctrl;
   }
 
   @override
   void initState(){
     super.initState();
     mapMarkers.add(Marker(
-      markerId: MarkerId('Teste'),
+      markerId: MarkerId('IC-UFF'),
       draggable: false,
       infoWindow: InfoWindow(title: 'IC-UFF'),
-      onTap: () {
-        print('IC-UFF');
-      },
       position: LatLng(-22.906463, -43.133291),
     ));
+    getCurrentLocation();
+  }
+
+  void addMarker(){
+    setState(() {
+     mapMarkers.add(Marker(
+        markerId: MarkerId(mapMarkers.length.toString()),
+        infoWindow: InfoWindow(
+          title: "Teste"
+        ),
+        position: LatLng(position.latitude, position.longitude),
+        draggable: false,
+        icon: BitmapDescriptor.defaultMarker,
+     )); 
+    });
+    print('teste');
+  }
+
+  //criando o widget do mapa
+  Widget MapWidget(){
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        myLocationEnabled: true,
+        markers: Set<Marker>.of(mapMarkers),
+        onMapCreated: onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(position.latitude, position.longitude), //Localização do usuário
+          zoom: 12.0
+        ),
+        //mapType: MapType.hybrid,
+      ),
+    );
   }
 
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mapa'),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: GoogleMap(
-            initialCameraPosition: CameraPosition(
-              target: LatLng(-22.906463, -43.133291), //Instituto de Computação UFF
-              zoom: 12.0
-            ),
-            myLocationEnabled: true,
-            markers: Set.from(mapMarkers),
-            //mapType: MapType.hybrid,
-          ),
+        leading: new IconButton(
+          icon: new Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: (){
+            Navigator.pop(context);
+          },
         ),
       ),
+      body: Center(
+        child: _map,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_location),
+        onPressed: addMarker,
+        backgroundColor: Colors.lightGreen,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
