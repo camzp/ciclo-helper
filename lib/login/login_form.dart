c1f594a24af155636f1496eba930a0651d6f8408
Andre <andmax94@gmail.com>
2019-11-27 02:37:46 -0300

    Desabilitei o botão de login do google enquanto não conseguimos a chave do firebase

--- lib/login/login_form.dart
+++ lib/login/login_form.dart
@@ -137,7 +137,7 @@ class _LoginFormState extends State<LoginForm> {
                               ? _onFormSubmitted
                               : null,
                         ),
-                        GoogleLoginButton(),
+//                        GoogleLoginButton(),
                         CreateAccountButton(userRepository: _userRepository),
                       ],
                     ),
